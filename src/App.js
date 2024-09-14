99184d5176e8bb32231a73b3dec3503f8d74f3de
luigi iadicola <luigiadicola@gmail.com>
2024-09-14 20:21:43 +0200

    Appuntamenti first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
+import { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'; 
+import 'bootstrap/dist/js/bootstrap.bundle.min'; 
+
+import List from './components/List';
+import FormDate from './components/FormDate';
 
 function App() {
+
+  const [allDate, setAllDate] = useState([]);
+
+ 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section >
+      <div className="container d-flex justify-content-center flex-column my-5 gap-3">
+        <h2>Prossimi Incontri</h2>
+        <div className='shadow px-2'>
+          <FormDate setAllDate={setAllDate}/>
+          <List allDate={allDate}/>
+
+        </div>
+
+
+      </div>
+    </section>
   );
 }
 
---
+++ src/components/FormDate.js
@@ -0,0 +1,73 @@
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+
+const FormDate = ({ setAllDate }) => {
+    const [appuntamento, setAppuntamento] = useState({
+        name: '',
+        phone: '',
+        datetime: '',
+    })
+
+    const handleSubit = (e) => {
+        e.preventDefault();
+        if (appuntamento.name && appuntamento.datetime && appuntamento.phone) {
+            setAllDate(prevAllDate => [
+                ...prevAllDate, //manienti gli appuntamenti esistenti
+                {
+                    id: Date.now().toString(),
+                    ...appuntamento, //inserisici i nuovi dati del form
+                }
+            ]);
+
+
+            setAppuntamento({ name: '', phone: '', datetime: '' });
+        } else {
+            alert('Insert All Data');
+        }
+    }
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setAppuntamento({ ...appuntamento, [name]: value });
+        
+
+    }
+
+    return (
+        <form className='p-5  rounded border border-secondary' onSubmit={handleSubit}>
+            <div className="form-group">
+                <label htmlFor="full-name">Name</label>
+                <input type="Full Name" name='name' className="form-control" id="full-name" placeholder="text"
+                    value={appuntamento.name}
+                    onChange={handleChange}
+
+                />
+            </div>
+
+            <div className="form-group">
+                <label htmlFor="tel">Phone number</label>
+                <input type="tel" name='phone' className="form-control" id="tel" placeholder="555-555-1234"
+                    value={appuntamento.phone}
+                    onChange={handleChange}
+                />
+                <small id="cellHelp" className="form-text text-muted">Minimum 10 numbers</small>
+            </div>
+
+            <div className="form-group">
+                <label htmlFor="datetime">Date and Time</label>
+                <input type="datetime-local" name='datetime' className="form-control" id="datetime"
+                    value={appuntamento.datetime}
+                    onChange={handleChange}
+                    min={new Date().toISOString().slice(0, 16)}
+                    max="2999-12-31T23:59"
+                />
+            </div>
+            <div className="d-flex justify-content-center">
+                <button type="submit" className="btn btn-primary w-75 my-2 shadow mt-5">Add Date</button>
+            </div>
+        </form>
+    )
+}
+
+export default FormDate
